21ae2ab50c8cffcd4b331c25b95a51120478feb7
ebrahims1902 <ebrahims1902@gmail.com>
2020-03-29 22:40:59 +0530

    sharing your location

--- public/js/chat.js
+++ public/js/chat.js
@@ -10,7 +10,13 @@ document.querySelector('#message-form').addEventListener('submit', (e)=>{
 
     const message = e.target.elements.message.value
 
-    socket.emit('sendMessage',message)
+    socket.emit('sendMessage',message, (error)=>{
+        if(error){
+            return console.log(error)
+        }
+
+        console.log('Message delivered')
+    })
 })
 
 document.querySelector('#send-location').addEventListener('click',()=>{
@@ -19,6 +25,11 @@ document.querySelector('#send-location').addEventListener('click',()=>{
     }
 
     navigator.geolocation.getCurrentPosition((position)=>{
-        console.log(position)
+        socket.emit('sendLocation',{
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        }, ()=>{
+            console.log('Location shared')
+        })
     })
 })
